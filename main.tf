726caf62e06148bc5bdfc9786715a72defa73877
dkchcl <dkc.hcl@gmail.com>
2025-07-12 21:31:55 +0530

    add st2

--- main.tf
+++ main.tf
@@ -2,3 +2,10 @@ resource "azurerm_resource_group" "name" {
   name = "example-resource-group"
     location = "West Europe"
 }
+resource "azurerm_storage_account" "name2" {
+  name                     = "examplestorageacct2"
+  resource_group_name      = azurerm_resource_group.name.name
+  location                 = azurerm_resource_group.name.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
