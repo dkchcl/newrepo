f0aca8f47bffbfd82e3012a3db6a376b8dd9b88c
dkchcl <dkc.hcl@gmail.com>
2025-07-13 23:45:38 +0530

    st2

--- main.tf
+++ main.tf
@@ -4,21 +4,13 @@ resource "azurerm_resource_group" "name" {
 }
 
 resource "azurerm_storage_account" "name2" {
-  name                     = "examplestorageacct"
+  name                     = "examplestorageacct2"
   resource_group_name      = azurerm_resource_group.name.name
   location                 = azurerm_resource_group.name.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
-
-resource "azurerm_storage_account" "name3" {
-  name                     = "examplestorageacct3"
-  resource_group_name      = azurerm_resource_group.name.name
-  location                 = azurerm_resource_group.name.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
 resource "azurerm_storage_account" "name1" {
   name                     = "examplestorageacct1"
   resource_group_name      = azurerm_resource_group.name.name
