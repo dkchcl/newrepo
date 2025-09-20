bcade03326c4999b91e791fbe71f32beae4fc51f
dkchcl <dkc.hcl@gmail.com>
2025-09-20 22:25:48 +0530

    Delete Modules directory

--- Modules/monitor_autoscale/data.tf
+++
@@ -1,4 +0,0 @@
-data "azurerm_virtual_machine_scale_set" "vmss" {
-  name                = var.vmss_name
-  resource_group_name = var.rg_name
-}
--- Modules/public_ip/data.tf
+++
@@ -1,4 +0,0 @@
-# data "azurerm_public_ip" "pip" {
-#   name                = var.public_ip_name
-#   resource_group_name = var.rg_name
-# }
