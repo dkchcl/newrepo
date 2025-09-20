b7092695912d64693608fecf86c079d8f3a283db
dkchcl <dkc.hcl@gmail.com>
2025-09-20 22:22:05 +0530

    Delete app_gateway directory

--- app_gateway/Modules/azurerm_virtual_network/main.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_virtual_network" "vnet" {
-  name                = var.vnet_name
-  resource_group_name = var.rg_name
-  location            = var.location
-  address_space       = var.address_space
-}
